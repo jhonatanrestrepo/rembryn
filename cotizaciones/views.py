1c48ad2c6b080faf04d9bfdb7e67d174421af38c
asdrubal22 <henry_rodriguez23201@elpoli.edu.co>
2022-12-08 13:56:52 -0500

    Avances

--- cotizaciones/views.py
+++ cotizaciones/views.py
@@ -7,10 +7,6 @@ from proyecto.models import Proyecto
 from django.shortcuts import render, redirect, get_object_or_404
 from django.core.mail import send_mail
 
-
-
-
-
 # Create your views here.
 
 class CotizacionProyecto(View):
@@ -18,20 +14,30 @@ class CotizacionProyecto(View):
         logged_in_user=request.user
         materiales = Asignacion_Material.objects.filter(proyecto__cliente__in=[logged_in_user.id])
 
+        print(materiales)
+
         proyect = get_object_or_404(Proyecto, cliente__in=[logged_in_user.id])
+
+        print(proyect)
+
+        print(request.user.email)
+
+        email = request.user.email
 
         precio = 0
         for materiale in materiales:
-            #print(materiale.material, " --> ",materiale.material.precio)
+            print(materiale.material, " --> ",materiale.material.precio)
             precio = precio + materiale.material.precio
+
+        print(precio)
             
-        p, created = Cotizacion.objects.get_or_create(proyecto=proyect,precio=precio)
-        p.save()
+        # p, created = Cotizacion.objects.get_or_create(proyecto=proyect,precio=precio)
+        # p.save()
 
         send_mail(
             subject="Cotización realizada",
             message="Realizaste la cotización de tu proyecto, esperamos que te haya gustado.",
-            recipient_list=['geirageuffajou-4499@yopmail.com'],
+            recipient_list=[email],
             from_email="Rembryn <henry_rodriguez23201@elpoli.edu.co>"
         )
 
